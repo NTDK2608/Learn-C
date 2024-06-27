71f49e0e65685cffa7ad3e7f259b1b88d51f63c3
Aoi_tori <61313633+NtdangK@users.noreply.github.com>
2024-06-27 09:50:56 +0700

    uoc_nguyen_to.c

--- uoc_nguyen_to.c
+++ uoc_nguyen_to.c
@@ -1,30 +1,18 @@
 #include<stdio.h>
 
+int prime(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
 int main(){
     int n;
-    printf("Hay nhap so nguyen n vao: ");
+    printf("Nhap n vao:");
     scanf("%d",&n);
-    int a[10000],k=0;
-    for (int i=2;i<=n;i++){
-        if(n%i==0){
-            a[k]=i;
-            k++;
-        }
-    }
-    // for (int z=0;z<k;z++){
-    //     printf("%d ",a[z]);
-    // }
-    printf("Cac uoc so nguyen to cua %d la: ",n);
-    for (int j=0;j<k;j++){
-        int temp=0;
-        for(int l=2;l*l<=a[j];l++){
-            if(a[j]%l==0){
-                temp++;
-            }
-        }
-        if(temp==0){
-            printf("%d ",a[j]);
-        }
+    for(int i=2;i<=n/2;i++){
+        //in các ước nguyên số của n ra 
+        if(prime(i)) printf("%d ",i);
     }
     return 0;
 }
